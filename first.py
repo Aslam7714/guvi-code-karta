2cf6d73c333e07121ff929ba0b942ccf13ca405c
Aslam7714 <42862345+Aslam7714@users.noreply.github.com>
2018-08-31 14:44:29 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,9 @@
+a=int(raw_input("enter the number to check whether positive or negative "))
+if(a>0):
+  print("the given number is positive")
+elif(a<0):
+  print("the given number is negative")
+elif(a==0):
+  print("the given number is zero")
+else:
+  print("invalid input")
