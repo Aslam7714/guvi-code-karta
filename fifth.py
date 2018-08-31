a02832319c902c88946f0b69775944bcb44dce03
Aslam7714 <42862345+Aslam7714@users.noreply.github.com>
2018-08-31 15:40:46 +0530

    Create fifth.py

---
+++ fifth.py
@@ -0,0 +1,9 @@
+a=int(raw_input("enter first number"))
+b=int(raw_input("enter second number"))
+c=int(raw_input("enter third number"))
+if(a>b and a>c):
+  print(a)
+elif(b>a and b>c):
+  print(b)
+else:
+  print(c)
